ff3d12908f42bdc0490c2b5c7b2088a680f6a734
Uzair Iqbal <uzair.qq@outlook.com>
2018-09-27 04:14:05 +0500

    Styling

--- AwesomeProject/src/components/Header.js
+++ AwesomeProject/src/components/Header.js
@@ -2,6 +2,19 @@ import React from 'react';
 import { Text } from 'react-native';
 
 
-const Header = () => (<Text>Some text UZAIR</Text>);
+const Header = () => {
+    const { textStyle } = styles;
+
+
+    return (
+        <Text style={textStyle}>Some text UZAIR</Text>
+    );
+}
+
+const styles = {
+    textStyle: {
+        fontSize: 100
+    }
+};
 
 export default Header;
