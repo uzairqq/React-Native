85e352bc5b5523c32ab90d83a222f8d14a38340f
Uzair Iqbal <uzair.qq@outlook.com>
2018-09-27 03:02:08 +0500

    Destructing imports and refactoring the component. Destructing means jo chahiye ho wohi uthao library se

--- AwesomeProject/index.js
+++ AwesomeProject/index.js
@@ -1,14 +1,10 @@
 // Import a library to help to create a component
 import React from 'react';
-import ReactNative from 'react-native';
+import { Text, AppRegistry } from 'react-native';
 
 
 //Create a component
-const App = () => {
-    return (
-        <Text>Some Text</Text>
-    );
-}
+const App = () => (<Text>Some Text</Text>);
 
 //Render that component
-ReactNative.AppRegistry.registerComponent('AwesomeProject', () => App);
+AppRegistry.registerComponent('AwesomeProject', () => App);
