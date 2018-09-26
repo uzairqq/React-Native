51333f1be8a5c5004cd6c23ed6e7b7e28eaf5f6c
Uzair Iqbal <uzair.qq@outlook.com>
2018-09-27 04:49:00 +0500

    Header Styling With Stype Properties

--- AwesomeProject/src/components/Header.js
+++ AwesomeProject/src/components/Header.js
@@ -6,17 +6,27 @@ const Header = () => {
     const { textStyle, viewStyle } = styles;
     return (
         <View style={viewStyle}>
-            <Text style={textStyle}>Some text UZAIR</Text>
+            <Text style={textStyle}>Albums!</Text>
         </View>
     );
 }
 
 const styles = {
     viewStyle: {
-        backgroundColor: 'green'
+        backgroundColor: '#F8F8F8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative'
+
     },
     textStyle: {
-        fontSize: 100
+        fontSize: 20,
     }
 };
 
