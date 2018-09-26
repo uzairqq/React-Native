280d2c6844b77e56c463a75768e38ffccfdd6674
Uzair Iqbal <uzair.qq@outlook.com>
2018-09-27 03:40:06 +0500

    Creating the Header Component and consuming it in the Root Component

--- AwesomeProject/index.js
+++ AwesomeProject/index.js
@@ -1,10 +1,11 @@
 // Import a library to help to create a component
 import React from 'react';
 import { Text, AppRegistry } from 'react-native';
+import Header from './src/components/Header';
 
 
 //Create a component
-const App = () => (<Text>Some Text</Text>);
+const App = () => (<Header />);
 
 //Render that component
 AppRegistry.registerComponent('AwesomeProject', () => App);
---
+++ AwesomeProject/src/components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+
+const Header = () => (<Text>Some text UZAIR</Text>);
+
+export default Header;
