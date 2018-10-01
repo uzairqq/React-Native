318c1e33da151fe03827ed8d657489ecaf2125bc
Uzair Iqbal <uzair.qq@outlook.com>
2018-10-02 00:32:45 +0500

    Displaying albumData artist and desinging the Header Image alag and Text alag

--- AwesomeProject/src/components/AlbumDetail.js
+++ AwesomeProject/src/components/AlbumDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
@@ -7,10 +7,24 @@ const AlbumDetail = (props) => {
     return (
         <Card>
             <CardSection>
-                <Text>{props.albumData.title}</Text>
+                <View>
+
+                </View>
+                <View style={styles.headerContentStyle}>
+                    <Text>{props.albumData.title}</Text>
+                    <Text>{props.albumData.artist}</Text>
+                </View>
+
             </CardSection>
         </Card>
     )
+}
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    }
 }
 
 export default AlbumDetail;
