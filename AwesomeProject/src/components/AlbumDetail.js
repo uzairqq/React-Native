07848035c713cd49686bf05f6041b33c81cb13eb
Uzair Iqbal <uzair.qq@outlook.com>
2018-10-02 00:56:50 +0500

    showed the image using Image properties and setted the style of image to show

--- AwesomeProject/src/components/AlbumDetail.js
+++ AwesomeProject/src/components/AlbumDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
@@ -8,7 +8,8 @@ const AlbumDetail = (props) => {
         <Card>
             <CardSection>
                 <View>
-
+                    <Image style={styles.thumbnailStyle}
+                        source={{ uri: props.albumData.thumbnail_image }} />
                 </View>
                 <View style={styles.headerContentStyle}>
                     <Text>{props.albumData.title}</Text>
@@ -24,7 +25,13 @@ const styles = {
     headerContentStyle: {
         flexDirection: 'column',
         justifyContent: 'space-around'
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
     }
+
+
 }
 
 export default AlbumDetail;
