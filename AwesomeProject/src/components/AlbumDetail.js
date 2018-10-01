af572dcb105e4f592d4e505f568d9a5ffc38d6c0
Uzair Iqbal <uzair.qq@outlook.com>
2018-10-02 03:02:48 +0500

    Created a Button Component And used it in AlbumDetails component and used TouchableOpacity for buttons

--- AwesomeProject/src/components/AlbumDetail.js
+++ AwesomeProject/src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ albumData }) => {
     const { title, artist, thumbnail_image, image } = albumData;
@@ -24,9 +25,14 @@ const AlbumDetail = ({ albumData }) => {
                     <Text>{artist}</Text>
                 </View>
             </CardSection>
+
             <CardSection>
                 <Image style={imageStyle}
                     source={{ uri: image }} />
+            </CardSection>
+
+            <CardSection>
+                <Button />
             </CardSection>
         </Card>
     )
