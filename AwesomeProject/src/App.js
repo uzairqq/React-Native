22489b5a0f37c6a55cf7a1f80c265155d55e1461
Uzair Iqbal <uzair.qq@outlook.com>
2018-10-10 00:51:10 +0500

    Created A Login From and using it it App.js

--- AwesomeProject/src/App.js
+++ AwesomeProject/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Header } from "./components/common";
+import { Header } from './components/common'
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <Text>An Starting Auth App</Text>
+                <LoginForm />
             </View>
         )
     }
