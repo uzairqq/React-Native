8845ca3cdc5938e197e70e3e07a0403a592852be
Uzair Iqbal <uzair.qq@outlook.com>
2018-10-10 01:52:00 +0500

    Creating Input Feild a Separate common component and passing props from Login form to Input Component

---
+++ AwesomeProject/src/components/common/Input.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, TextInput } from 'react-native';
+
+
+const Input = ({ label, value, onChangeText }) => {
+    return (
+        <View>
+            <Text>{label}</Text>
+            <TextInput
+                value={value}
+                onChangeText={onChangeText}
+                style={{ height: 20, width: 100 }}
+            />
+        </View>
+    )
+}
+
+export { Input };
