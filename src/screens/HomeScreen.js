6c2e90743c828acb881b2429d63a90f404d8eeee
Muhammad uzair <uzairiqbal@Muhammads-MacBook-Pro.local>
2019-10-06 13:09:16 +0500

    expo up and working and checked

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+  return <Text style={styles.text}>HomeScreen and uzair</Text>;
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30
+    fontSize: 30,
+    fontStyle:'italic'
+  
   }
 });
 
