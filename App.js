0e942e7d77bfda33c0bbed3857bc97dfd325d212
Muhammad uzair <uzairiqbal@Muhammads-MacBook-Pro.local>
2019-10-08 16:15:01 +0500

    inline styling of a component

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import HomeScreen from './src/screens/HomeScreen';
 import ComponentScreen from './src/screens/ComponentScreen';
-import UzairComponent from './src/screens/UzairComponent';
 
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
     Components:ComponentScreen,
-    Seconds:UzairComponent
   },
   {
-    initialRouteName: 'Seconds',
+    initialRouteName: 'Components',
     defaultNavigationOptions: {
       title: 'App'
     }
--- src/screens/ComponentScreen.js
+++ src/screens/ComponentScreen.js
@@ -3,14 +3,14 @@ import {Text,StyleSheet} from 'react-native';
 
 
 const ComponentScreen=()=>{
-    return <Text style={styles.textStyle}>Hi There from Component Screen</Text>;
+    return <Text style={{fontSize:30,color:'red'}}>Hi There from Component Screen</Text>;
 };
 
-const styles=StyleSheet.create({
-    textStyle:{
-        fontSize:40,
-        color:'blue'
-    }
-});
+// const styles=StyleSheet.create({
+//     textStyle:{
+//         fontSize:40,
+//         color:'blue'
+//     }
+// });
 
 export default ComponentScreen;
