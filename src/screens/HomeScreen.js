34920454a2b60a00c2d90706ee12bada03fccdd5
Muhammad uzair <uzairiqbal@Muhammads-MacBook-Pro.local>
2019-10-09 14:02:24 +0500

    destruction of props

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,19 +2,18 @@ import React,{Fragment} from 'react';
 import { Text, StyleSheet,View,Button,TouchableOpacity } from 'react-native';
 import { ComponentScreen } from "./ComponentScreen";
 
-const HomeScreen = (props) => {
-// console.log(props.navigation);
+const HomeScreen = ({navigation}) => {
 
   return (
   
     <View>
   <Text style={styles.text}>HomeScreen and uzair</Text>
-   <Button title="Go to Components Demo" onPress={()=>props.navigation.navigate('Components')}/>
-   <Button title="Go to Excercise Demo" onPress={()=>props.navigation.navigate('Excercise')}/>
-   <Button title="Go to FlatList Demo" onPress={()=>props.navigation.navigate('FlatList')}/>
-   <Button title="Go to List Demo" onPress={()=>props.navigation.navigate('List')}/>
+   <Button title="Go to Components Demo" onPress={()=>navigation.navigate('Components')}/>
+   <Button title="Go to Excercise Demo" onPress={()=>navigation.navigate('Excercise')}/>
+   <Button title="Go to FlatList Demo" onPress={()=>navigation.navigate('FlatList')}/>
+   <Button title="Go to List Demo" onPress={()=>navigation.navigate('List')}/>
 
-    <TouchableOpacity onPress={()=>{props.navigation.navigate('Components')}}>
+    <TouchableOpacity onPress={()=>{navigation.navigate('Components')}}>
       <Text>Go to Components Demo</Text>
     </TouchableOpacity>
 
