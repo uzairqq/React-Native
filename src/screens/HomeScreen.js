bff1fa463bf9d71f047cbebe6d7abe8ef06df223
Muhammad uzair <uzairiqbal@Muhammads-MacBook-Pro.local>
2019-10-09 14:07:22 +0500

    more deeper on destructing props

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,18 +2,18 @@ import React,{Fragment} from 'react';
 import { Text, StyleSheet,View,Button,TouchableOpacity } from 'react-native';
 import { ComponentScreen } from "./ComponentScreen";
 
-const HomeScreen = ({navigation}) => {
+const HomeScreen = ({navigation: {navigate}}) => {
 
   return (
   
     <View>
   <Text style={styles.text}>HomeScreen and uzair</Text>
-   <Button title="Go to Components Demo" onPress={()=>navigation.navigate('Components')}/>
-   <Button title="Go to Excercise Demo" onPress={()=>navigation.navigate('Excercise')}/>
-   <Button title="Go to FlatList Demo" onPress={()=>navigation.navigate('FlatList')}/>
-   <Button title="Go to List Demo" onPress={()=>navigation.navigate('List')}/>
+   <Button title="Go to Components Demo" onPress={()=>navigate('Components')}/>
+   <Button title="Go to Excercise Demo" onPress={()=>navigate('Excercise')}/>
+   <Button title="Go to FlatList Demo" onPress={()=>navigate('FlatList')}/>
+   <Button title="Go to List Demo" onPress={()=>navigate('List')}/>
 
-    <TouchableOpacity onPress={()=>{navigation.navigate('Components')}}>
+    <TouchableOpacity onPress={()=>{navigate('Components')}}>
       <Text>Go to Components Demo</Text>
     </TouchableOpacity>
 
