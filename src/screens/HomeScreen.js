fd0832f6bc6062df509e28af95285dd70f1bb84c
Muhammad uzair <uzairiqbal@Muhammads-MacBook-Pro.local>
2019-10-09 13:52:04 +0500

    navigation with react navigation

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,16 +3,16 @@ import { Text, StyleSheet,View,Button,TouchableOpacity } from 'react-native';
 import { ComponentScreen } from "./ComponentScreen";
 
 const HomeScreen = (props) => {
-console.log(props.navigation);
+// console.log(props.navigation);
 
   return (
   
     <View>
   <Text style={styles.text}>HomeScreen and uzair</Text>
-   <Button title="Go to Components Demo" onPress={()=>console.log('Components Demo Pressed')}/>
-   <Button title="Go to Excercise Demo" onPress={()=>console.log('Excercise Demo Pressed')}/>
-   <Button title="Go to FlatList Demo" onPress={()=>console.log('FlatList Demo Pressed')}/>
-   <Button title="Go to List Demo" onPress={()=>console.log('List Demo Pressed')}/>
+   <Button title="Go to Components Demo" onPress={()=>props.navigation.navigate('Components')}/>
+   <Button title="Go to Excercise Demo" onPress={()=>props.navigation.navigate('Excercise')}/>
+   <Button title="Go to FlatList Demo" onPress={()=>props.navigation.navigate('FlatList')}/>
+   <Button title="Go to List Demo" onPress={()=>props.navigation.navigate('List')}/>
 
     <TouchableOpacity onPress={()=>{console.log('Touchable Components Demo')}}>
       <Text>Go to Components Demo</Text>
