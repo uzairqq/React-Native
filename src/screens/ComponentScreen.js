4f99edefe3b1531b2989611dae6bcfcef7ccf532
Muhammad uzair <uzairiqbal@Muhammads-MacBook-Pro.local>
2019-10-13 16:21:18 +0500

    Created simple re-usable component image details used in image screen

--- src/screens/ComponentScreen.js
+++ src/screens/ComponentScreen.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import {Text,StyleSheet,View} from 'react-native';
+import {
+    Text,
+    StyleSheet,
+    View
+} from 'react-native';
 
 
-const ComponentScreen=()=>{
+const ComponentScreen = () => {
 
-    const greeting=<Text>hello there! My name is Uzair</Text>
+    const greeting = < Text > hello there!My name is Uzair < /Text>
 
-    return( 
-    <View>
-        <Text style={styles.textStyle}>Hi There from Component Screen</Text>
-        {greeting}
-        </View>
-        )
+    return ( <
+        View >
+        <
+        Text style = {
+            styles.textStyle
+        } > Hi There from Component Screen < /Text> {
+            greeting
+        } <
+        /View>
+    )
 };
 
-const styles=StyleSheet.create({
-    textStyle:{
-        fontSize:40,
-        color:'blue'
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize: 40,
+        color: 'blue'
     },
 });
 
