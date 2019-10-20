2573c069cd15c95b2ff6d8ac26d5979bdeeffb25
Muhammad uzair <uzairiqbal@Muhammads-MacBook-Pro.local>
2019-10-20 12:40:48 +0500

    Re-usable Helpers functions

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -10,12 +10,24 @@ const SquareScreen = () => {
   const [blue, setBlue] = useState(0);
 
   const setColor = (color, change) => {
-    if (color === "red") {
-      if (red + change > 255 || red + change < 0) {
+    switch (color) {
+      case "red":
+        red + change > 255 || red + change < 0 ? null : setRed(red + change);
         return;
-      } else {
-        setRed(red + change);
-      }
+
+      case "green":
+        green + change > 255 || green + change < 0
+          ? null
+          : setGreen(green + change);
+        return;
+      case "blue":
+        blue + change > 255 || blue + change < 0
+          ? null
+          : setBlue(blue + change);
+        return;
+
+      default:
+        return;
     }
   };
 
@@ -31,13 +43,13 @@ const SquareScreen = () => {
       />
       <ColorCounter
         color="Green"
-        onIncrease={() => setGreen(green + COLOR_INCREMENT)}
-        onDecrease={() => setGreen(green - COLOR_INCREMENT)}
+        onIncrease={() => setColor("green", COLOR_INCREMENT)}
+        onDecrease={() => setColor("green", -1 * COLOR_INCREMENT)}
       />
       <ColorCounter
         color="Blue"
-        onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
-        onDecrease={() => setBlue(blue - COLOR_INCREMENT)}
+        onIncrease={() => setColor("blue", COLOR_INCREMENT)}
+        onDecrease={() => setColor("blue", -1 * COLOR_INCREMENT)}
       />
       <View
         style={{
