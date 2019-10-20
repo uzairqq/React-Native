acbf82ffa9a583479e7543df4063363a39ddeea5
Muhammad uzair <uzairiqbal@Muhammads-MacBook-Pro.local>
2019-10-20 11:46:51 +0500

    Color Increments with functional state and const vars and child re-usable components

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 import ColorCounter from "../components/ColourCounter";
 
+const COLOR_INCREMENT = 50;
+
 const SquareScreen = () => {
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+
+  console.log("Red", red);
+  console.log("Green", green);
+  console.log("Blue", blue);
   return (
     <View>
-      <ColorCounter color="Red" />
-      <ColorCounter color="Green" />
-      <ColorCounter color="Blue" />
+      <ColorCounter
+        color="Red"
+        onIncrease={() => setRed(red + COLOR_INCREMENT)}
+        onDecrease={() => setRed(red - COLOR_INCREMENT)}
+      />
+      <ColorCounter
+        color="Green"
+        onIncrease={() => setGreen(green + COLOR_INCREMENT)}
+        onDecrease={() => setGreen(green - COLOR_INCREMENT)}
+      />
+      <ColorCounter
+        color="Blue"
+        onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
+        onDecrease={() => setBlue(blue - COLOR_INCREMENT)}
+      />
+      <View
+        style={{
+          width: 150,
+          height: 100,
+          backgroundColor: `rgb(${red},${green},${blue})`
+        }}
+      />
     </View>
   );
 };
