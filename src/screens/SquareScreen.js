c3ec618ee2c1b6758003eb78be5a0863d429365c
Muhammad uzair <uzairiqbal@Muhammads-MacBook-Pro.local>
2019-11-07 00:39:29 +0500

    userd ternary operations for if else in reducers validation

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -7,23 +7,20 @@ const COLOR_INCREMENT = 50;
 const reducer = (state, action) => {
   switch (action.colorToChange) {
     case "red":
-      if (state.red + action.amount > 255 || state.red + action.amount < 0) {
-        return state;
-      }
-      return { ...state, red: state.red + action.amount };
+      return state.red + action.amount > 255 || state.red + action.amount < 0
+        ? state
+        : { ...state, red: state.red + action.amount };
+
     case "green":
-      if (
-        state.green + action.amount > 255 ||
+      return state.green + action.amount > 255 ||
         state.green + action.amount < 0
-      ) {
-        return state;
-      }
-      return { ...state, green: state.green + action.amount };
+        ? state
+        : { ...state, green: state.green + action.amount };
+
     case "blue":
-      if (state.blue + action.amount > 255 || state.blue + state.amount < 0) {
-        return state;
-      }
-      return { ...state, blue: state.blue + action.amount };
+      return state.blue + action.amount > 255 || state.blue + state.amount < 0
+        ? state
+        : { ...state, blue: state.blue + action.amount };
     default:
       return state;
   }
