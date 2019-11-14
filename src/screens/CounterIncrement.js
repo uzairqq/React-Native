c3ba891b0b449759c78077ff7611613fa22d7c86
Muhammad uzair <uzairiqbal@Muhammads-MacBook-Pro.local>
2019-11-15 01:03:09 +0500

    Used useReducer For Excercise

--- src/screens/CounterIncrement.js
+++ src/screens/CounterIncrement.js
@@ -1,24 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 import { View, Text, StyleSheet, Button, TouchableOpacity } from "react-native";
+import { DrawerActions } from "react-navigation";
+
+const reducer = (state, action) => {
+  //state === { counter:number }
+  //howToChangeState ya phr action === { type: 'increment' || 'decrement'}    , payload=1
+
+  switch (action.type) {
+    case "increment":
+      return { ...state, count: state.count + action.payload };
+    case "decrement":
+      return { ...state, count: state.count - action.payload };
+    default:
+      return state;
+  }
+};
 
 const CounterIncrement = () => {
-  const [counter, setCounter] = useState(0);
-
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
   return (
     <View>
       <Button
         title="Increment"
         onPress={() => {
-          setCounter(counter + 1);
+          dispatch({ type: "increment", payload: 1 });
         }}
       ></Button>
       <Button
         title="Decrement"
         onPress={() => {
-          setCounter(counter - 1);
+          dispatch({ type: "decrement", payload: 1 });
         }}
       ></Button>
-      <Text>Current Count: {counter}</Text>
+      <Text>Current Count: {state.count}</Text>
     </View>
   );
 };
